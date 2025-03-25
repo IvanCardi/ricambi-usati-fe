d5ded871363775c971a9a894c5c8f78778c8bbb7
IvanCardi <ivancardillo1996@gmail.com>
2025-03-25 17:38:55 +0100

    feat: adds call to be in get car parts

--- src/app/(sections)/shop/page.tsx
+++ src/app/(sections)/shop/page.tsx
@@ -1,8 +1,22 @@
 import SearchFilters from "@/app/components/searchFilters";
 import Form from "./form";
 import CarPartsSection from "./carPartsSection";
+import { cookies } from "next/headers";
 
-export default function Shop() {
+const getProducts = async () => {
+  const token = (await cookies())?.get("access_token")?.value;
+  const products = await fetch(`${process.env.BE_BASE_URL}/carParts`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return await products.json();
+};
+
+export default async function Shop() {
+  const products = await getProducts();
+
   return (
     <main className="flex flex-col w-full justify-center items-center">
       <div className="flex w-full justify-center py-14">
