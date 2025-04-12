889a432d852adfde4f04ad1ab50945008f2e7d8d
Robel.Abraham <robel.abraham@snapp.ie>
2025-04-12 12:54:05 +0200

    fix: fixes cart actions logic

--- src/app/cart/cartContext.tsx
+++ src/app/cart/cartContext.tsx
@@ -33,18 +33,22 @@ const CartContext = createContext<CartContextType>({
 const CART_KEY = "my_cart";
 
 export const CartProvider = (props: PropsWithChildren) => {
-  const [cart, setCart] = useState<CartItem[]>(() => {
-    if (typeof window !== "undefined") {
-      // We are in the browser
-      const savedCart = localStorage.getItem(CART_KEY);
-      return savedCart ? JSON.parse(savedCart) : [];
-    }
-    return [];
-  });
+  const [cart, setCart] = useState<CartItem[]>([]);
+  const [isHydrated, setIsHydrated] = useState(false);
 
   useEffect(() => {
+    const savedCart = localStorage.getItem(CART_KEY);
+    if (savedCart) {
+      setCart(JSON.parse(savedCart));
+    }
+
+    setIsHydrated(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isHydrated) return;
     localStorage.setItem(CART_KEY, JSON.stringify(cart));
-  }, [cart]);
+  }, [cart, isHydrated]);
 
   const addToCart = (carPart: CarPart) => {
     setCart((prev) => {
@@ -61,25 +65,14 @@ export const CartProvider = (props: PropsWithChildren) => {
       }
     });
 
-    toast("L'articolo è stato aggiunto al carrello", {
-      action: {
-        label: "Undo",
-        onClick: () => removeFromCart(carPart.id),
-      },
-    });
+    toast("L'articolo è stato aggiunto al carrello");
   };
 
   const removeFromCart = (id: string) => {
     const cartItem = cart.find((item) => item.id === id);
 
     if (cartItem) {
-      setCart((prev) =>
-        cartItem.quantity > 1
-          ? prev.map((item) =>
-              item.id === id ? { ...item, quantity: item.quantity - 1 } : item
-            )
-          : prev.filter((item) => item.id !== id)
-      );
+      setCart((prev) => prev.filter((item) => item.id !== id));
 
       toast("L'articolo è stato rimosso dal carrello", {
         action: {
@@ -91,14 +84,25 @@ export const CartProvider = (props: PropsWithChildren) => {
   };
 
   const decrementItem = (id: string) => {
-    setCart((prev) => {
-      return prev.map((item) => {
-        if (item.id === id && item.quantity > 1) {
-          return { ...item, quantity: item.quantity - 1 };
-        }
-        return item;
+    const cartItem = cart.find((item) => item.id === id);
+
+    if (cartItem) {
+      setCart((prev) => {
+        return prev.map((item) => {
+          if (item.id === id && item.quantity > 1) {
+            return { ...item, quantity: item.quantity - 1 };
+          }
+          return item;
+        });
       });
-    });
+
+      toast("L'articolo è stato rimosso dal carrello", {
+        action: {
+          label: "Undo",
+          onClick: () => addToCart(cartItem),
+        },
+      });
+    }
   };
 
   const clearCart = () => {
