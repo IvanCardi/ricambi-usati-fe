0474ca354ca1ea0847774d52e151cc466eacb29f
Robel.Abraham <robel.abraham@snapp.ie>
2025-04-18 12:45:30 +0200

    feat: adds mobile version for cart item

--- src/app/shop/car-part-card.tsx
+++ src/app/shop/car-part-card.tsx
@@ -16,7 +16,7 @@ export function CarPartCard(carPart: CarPart) {
       <div className="flex flex-col justify-between items-center w-full h-full border-[3px] border-[#0BB489]">
         <div
           onClick={() => router.push(`/shop/product/${carPart.id}`)}
-          className="cursor-pointer flex w-full h-[205px] justify-center border-[3px] border-[#0BB489]"
+          className="cursor-pointer flex w-full h-[60%] justify-center border-[3px] border-[#0BB489]"
         >
           {carPart.imageUrl ? (
             <Image
@@ -37,27 +37,29 @@ export function CarPartCard(carPart: CarPart) {
             />
           )}
         </div>
-        <div className="flex flex-col w-full items-center border border-t-2 border-[#0BB489] gap-2 py-2">
-          <div className="flex flex-col items-center justify-center gap-2 w-full">
+        <div className="flex flex-col h-[40%] w-full items-center justify-between border border-t-2 border-[#0BB489] gap-2 py-2">
+          <div className="flex h-full items-center justify-center">
             <span
               onClick={() => router.push(`/shop/product/${carPart.id}`)}
               className="cursor-pointer text-base text-center font-inter font-semibold"
             >
               {carPart.name}
             </span>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-2 w-full">
             <div className="w-10 h-[1px] bg-black" />
             <span className="text-sm text-center font-inter font-semibold">
               {carPart.price}€
             </span>
+            <Button
+              className="w-fit bg-[#0BB489] hover:bg-[#0BB489]/85"
+              onClick={() => addToCart(carPart)}
+            >
+              <span className="text-[9px] text-white font-inter font-medium">
+                Aggiungi al carrello
+              </span>
+            </Button>
           </div>
-          <Button
-            className="w-fit bg-[#0BB489] hover:bg-[#0BB489]/85"
-            onClick={() => addToCart(carPart)}
-          >
-            <span className="text-[9px] text-white font-inter font-medium">
-              Aggiungi al carrello
-            </span>
-          </Button>
         </div>
       </div>
     </div>
